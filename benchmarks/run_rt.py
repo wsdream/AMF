b375f2e43112fbdbdba7f916d08a02810ed4a89d
jimzhu <jmzhu.sz@gmail.com>
2016-02-16 15:33:54 +0800

    update code

--- benchmarks/run_rt.py
+++ benchmarks/run_rt.py
@@ -21,7 +21,7 @@ para = {'dataPath': '../data/',
         'dataType': 'rt', # set the dataType as 'rt' or 'tp'
         'outPath': 'result/',
         'metrics': ['MAE', 'MRE', 'NPRE'], # delete where appropriate      
-        'density': np.arange(0.05, 0.06, 0.05), # matrix density
+        'density': np.arange(0.05, 0.31, 0.05), # matrix density
         'rounds': 1, # how many runs are performed at each matrix density
         'dimension': 10, # dimenisionality of the latent factors
         'eta': 0.8, # learning rate
@@ -29,7 +29,7 @@ para = {'dataPath': '../data/',
         'maxIter': 50, # the max iterations
         'convergeThreshold': 7e-3, # stopping criteria for convergence
         'beta': 0.3, # the controlling weight of exponential moving average
-        'saveTimeInfo': False, # whether to keep track of the running time
+        'saveTimeInfo': True, # whether to keep track of the running time
         'saveLog': True, # whether to save log into file
         'debugMode': False, # whether to record the debug info
         'parallelMode': True # whether to leverage multiprocessing for speedup
@@ -43,7 +43,7 @@ logger.info('AMF: Adaptive Matrix Factorization [TPDS]')
 
 # load the dataset
 dataTensor = dataloader.load(para)
-# dataTensor = dataTensor[0:50][0:50][0:50]
+
 # evaluate QoS prediction algorithm
 evaluator.execute(dataTensor, para)
 
--- benchmarks/run_tp.py
+++ benchmarks/run_tp.py
@@ -1,36 +1,27 @@
 ########################################################
-# run_tp.py: throughput prediction 
+# run_rt.py 
 # Author: Jamie Zhu <jimzhu@GitHub>
 # Created: 2014/2/6
-# Last updated: 2015/8/4
-# Implemented approach: AMF
-# Evaluation metrics: MAE, NMAE, RMSE, MRE, NPRE
+# Last updated: 2016/02/15
+# Implemented approach: AMF (Adaptive Matrix Factorization)
 ########################################################
 
 import numpy as np
 import os, sys, time
 import multiprocessing
-sys.path.append('src')
-# import C++ module
-try:
-    import core
-except ImportError, e:
-    print 'ImportError: ', e
-    os.system('python setup.py build_ext --inplace')
-from utilities import *
-import evaluator
-import dataloader
-import resulthandler
+from commons.utils import logger
+from commons import utils
+from commons import evaluator
+from commons import dataloader
+ 
 
-
-#########################################################
-# config area
-#
-para = {'dataType': 'tp', # choose 'tp' for throughput prediction
-        'dataPath': '../data/dataset#2/',
-        'outPath': 'result/raw/',
-        'metrics': ['MAE', 'NMAE', 'RMSE', 'MRE', 'NPRE'], # delete where appropriate       
-        'density': list(np.arange(0.05, 0.31, 0.05)), # matrix density
+# parameter config area
+para = {'dataPath': '../data/',
+        'dataName': 'dataset#2',
+        'dataType': 'tp', # set the dataType as 'rt' or 'tp'
+        'outPath': 'result/',
+        'metrics': ['MAE', 'MRE', 'NPRE'], # delete where appropriate      
+        'density': np.arange(0.05, 0.31, 0.05), # matrix density
         'rounds': 1, # how many runs are performed at each matrix density
         'dimension': 10, # dimenisionality of the latent factors
         'eta': 0.8, # learning rate
@@ -38,41 +29,23 @@ para = {'dataType': 'tp', # choose 'tp' for throughput prediction
         'maxIter': 50, # the max iterations
         'convergeThreshold': 2.5e-3, # stopping criteria for convergence
         'beta': 0.3, # the controlling weight of exponential moving average
-        'saveTimeInfo': False, # whether to keep track of the running time
+        'saveTimeInfo': True, # whether to keep track of the running time
         'saveLog': True, # whether to save log into file
         'debugMode': False, # whether to record the debug info
         'parallelMode': True # whether to leverage multiprocessing for speedup
         }
 
-initConfig(para)
-#########################################################
 
-
-startTime = time.clock() # start timing
+startTime = time.time() # start timing
+utils.setConfig(para) # set configuration
 logger.info('==============================================')
-logger.info('AMF: Adaptive Matrix Factorization [ICDCS\'14].')
+logger.info('AMF: Adaptive Matrix Factorization [TPDS]')
 
 # load the dataset
 dataTensor = dataloader.load(para)
 
-# run for each density
-if para['parallelMode']: # run on multiple processes
-    pool = multiprocessing.Pool()
-    for density in para['density']:
-        for roundId in xrange(para['rounds']):
-            pool.apply_async(evaluator.execute, (dataTensor, density, roundId, para))
-    pool.close()
-    pool.join()
-else: # run on single processes
-    for density in para['density']:
-        for roundId in xrange(para['rounds']):
-            evaluator.execute(dataTensor, density, roundId, para)
+# evaluate QoS prediction algorithm
+evaluator.execute(dataTensor, para)
 
-# result handling
-numTimeSlice = dataTensor.shape[2]
-resulthandler.process(para, numTimeSlice)
-
-logger.info(time.strftime('All done. Total running time: %d-th day - %Hhour - %Mmin - %Ssec.',
-         time.gmtime(time.clock() - startTime)))
+logger.info('All done. Elaspsed time: ' + utils.formatElapsedTime(time.time() - startTime)) # end timing
 logger.info('==============================================')
-sys.path.remove('src')
